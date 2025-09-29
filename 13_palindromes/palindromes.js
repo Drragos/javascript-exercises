69fb9ee13201aba971628f84b85009efd403a1db
Dragos Rusu <86013739+Drragos@users.noreply.github.com>
2025-09-29 21:33:48 +0300

    resolve exercises 12-13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let newString = [];
+  let backString = [];
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (
+      ![" ", "!", ".", ","].includes(string[i]) &&
+      typeof string != "number"
+    ) {
+      newString += string[i].toLowerCase();
+    } else continue;
+  }
+  for (let i = newString.length - 1; i >= 0; i--) {
+    backString += newString[i];
+  }
 
+  if (newString === backString) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
