77695bbc6761969b82429fb3fc4c17f7439a84fb
Dragos Rusu <86013739+Drragos@users.noreply.github.com>
2025-09-30 23:03:40 +0300

    rezolved all exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (x) {
+  x.sort(function (a, b) {
+    if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+      return "2025" - b.yearOfBirth - ("2025" - a.yearOfBirth);
+    } else {
+      return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+    }
+  });
+  x.map((person) => person.name);
+  return x[0];
 };
 
 // Do not edit below this line
